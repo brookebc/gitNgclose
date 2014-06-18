70e98f5af8dc1947ffee89081f8dec1810bd1ea6
brookebc <brooke.casey@gmail.com>
2014-06-18 07:13:10 -0400

    moved styles so below the build so they wont be removed

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -13,7 +13,7 @@ angular.module('gitNgcloseApp.directives')
             //create a CloudMade tile layer and add it to the map
             L.tileLayer('http://tile.stamen.com/terrain/{z}/{x}/{y}.png', {
                 attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
-                maxZoom: '18',
+                // maxZoom: '18',
                 opacity: 0.7,
                 detectRetina: true,
                 unloadInvisibleTiles: true,
